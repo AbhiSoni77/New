b038f478f0112814f52eeeab6316167e6edbc86e
Abhi <abhisoni38@gmil.com>
2025-01-03 01:51:27 +0530

    test

---
+++ Jan25/Python_basics/Lab01.py
@@ -0,0 +1,36 @@
+# print("Shree Radhey")
+#
+# print('abhi','soni',123,3434,"job","automation",sep='--',end='\n ')
+# print('abhi','soni',123,3434,"job","automation")
+#
+# import keyword
+# print(keyword.kwlist)
+
+# age =34
+# age =40
+#
+# print(age)
+#
+# complex_number= 2+3j
+# print(complex_number.real)
+# print(complex_number.imag)
+#
+# print(type(complex_number))
+
+
+# a,b,c,d,e =2,5,7,9,5
+# print(a,b,c,d,e,sep=("|"))
+
+# #99,100
+# result=max(99,100)
+# print(result)
+#
+# #99,100
+# result=min(99,100)
+# print(result)
+
+result=min(99,200,400,1000,100,-1,-100)
+print(result)
+
+result=max(99,200,400,1000,100,-1,-100)
+print(result)
